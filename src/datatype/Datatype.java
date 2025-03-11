b57ae7de5f067232078f6a448eac036e157a6987
RMK2410 <kadadiramesh33@gmail.com>
2025-03-11 08:44:01 +0530

    6th march

---
+++ src/datatype/Datatype.java
@@ -0,0 +1,21 @@
+package datatype;
+
+public class Datatype {
+    public static void main(String[] args) {
+        byte a=10;
+        int b=20,i=30, z=40;
+        char c='A';
+        long l=100000000000L;
+        double d=235678.8888888;
+        float f=234.78F;
+        System.out.println("byte is " + a);
+        System.out.println("int is "+ b + i + z);
+        System.out.println("char is "+c);
+        System.out.println("long is " +l);
+        System.out.println("double is " +d);
+        System.out.println("float is " +f);
+
+
+
+    }
+}
---
+++ src/datatype/Table.java
@@ -0,0 +1,30 @@
+package datatype;
+
+import java.io.PrintStream;
+
+public class Table {
+    public static void main(String[] args) {
+        int a=9;
+        System.out.printf("%d * %d =%d",a,1,(a*1));
+        System.out.println();
+        System.out.printf("%d * %d =%d", a,2,(a*2));
+        System.out.println();
+        System.out.printf("%d * %d =%d", a,3,(a*3));
+        System.out.println();
+        System.out.printf("%d * %d =%d", a,4,(a*4));
+        System.out.println();
+        System.out.printf("%d * %d =%d", a,5,(a*5));
+        System.out.println();
+        System.out.printf("%d * %d =%d", a,6,(a*6));
+        System.out.println();
+        System.out.printf("%d * %d =%d", a,7,(a*7));
+        System.out.println();
+        System.out.printf("%d * %d =%d", a,8,(a*8));
+        System.out.println();
+        System.out.printf("%d * %d =%d", a,9,(a*9));
+        System.out.println();
+        System.out.printf("%d * %d =%d", a,10,(a*10));
+
+
+    }
+}
