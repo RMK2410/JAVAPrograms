6ec3d78d590a66caeb9e16decd57abd833d34caf
RMK2410 <kadadiramesh33@gmail.com>
2025-03-12 09:08:51 +0530

    12th March Math functions

---
+++ src/MathFunctions.java
@@ -0,0 +1,7 @@
+public class MathFunctions {
+    public static void main(String[] args) {
+        double X = 10, Y = 10, Z = 10 , Result;
+        Result= Math.cbrt(Math.pow(X,2)+Math.pow(Y,2)+Math.abs(Z));
+        System.out.println(Result);
+    }
+}
